6b1f104c9d3471cee8842e23b7b401d0313dc106
Sergio Bispo <serjolasrg@gmail.com>
2024-07-15 17:44:15 -0300

    Criação projeto Santander Foorça de vendas

---
+++ src/main/java/sv/sistemas/SistemasApplication.java
@@ -0,0 +1,13 @@
+package sv.sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/sistemas/SistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
